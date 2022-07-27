0370485b0f91b7a48d977ea4e732539fb882fed8
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-27 14:20:47 +0300

    importing rapid Api url

---
+++ src/fetch-staffs/fetchData.js
@@ -0,0 +1,15 @@
+ export const ExerciseOptions =  {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/target/%7Btarget%7D',
+  headers: {
+    'X-RapidAPI-Key': ,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+ 
+ export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+ }
