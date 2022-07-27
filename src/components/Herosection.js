416847465359961232c975c9179fb7ecb1aea67c
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-27 13:07:55 +0300

    adding con tent in searcsection

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,11 +1,11 @@
 import React from 'react';  
 import { Box, Stack, Typography } from '@mui/material';
-import 
+import Logo from '../assets/img/Logo.jpeg';
 
 const Herosection = () => {
   return (
     <div>
-         <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+       <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
@@ -20,10 +20,10 @@ const Herosection = () => {
     <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
     </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+    <img src={Logo} alt="hero-banner" className="hero-banner-img" />
   </Box>
     </div>
   )
 }
 
-export default Herosection
+export default Herosection;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <div>
+        <Stack 
+          alignItems='center'
+          justifyContent='center'
+          p='20px'
+          mt='37px'
+        >
+            <Typography
+              fontWeight='900'
+              sx={{
+                fontsize: {lg: '44px', xs: '30px'
+            }}}
+            mb='50px'
+            textAlign={'center'}
+            >
+             Try it yourself:
+            </Typography>
+            <Box position={'relative'} mb={'720px'}>
+                <TextField 
+                    sx={{ 
+                      input: { 
+                      fontWeight: '700', 
+                      border: 'none', 
+                      borderRadius: '0px' 
+                      }, 
+                      width: { 
+                      lg: '700px', 
+                      xs: '350px' 
+                      }, 
+                      backgroundColor: 'grey', 
+                      borderRadius: '40px' 
+                    }}
+                  type={'text'}
+                  placeholder={'Exercises'}
+                  value={''}
+                  onchange= {(e) => {}}
+                  height={'76px'}
+                /> 
+            </Box>    
+        </Stack>
+    </div>
   )
 }
 
-export default SearchExercises
+export default SearchExercises 
