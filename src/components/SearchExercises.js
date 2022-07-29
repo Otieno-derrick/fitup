d196637a45bed8e70ef763fdfa7f9f8efe5b349e
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-29 07:39:26 +0300

    implementing the useeffect in searchexexsices

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -7,13 +7,40 @@ import ScrollBar from './ScrollBar';
 
 
 
-export default function SearchExercises({setExercises, bodyPart, setBodyPart }) {
+
+
+export default function SearchExercises({ setExercises, bodyPart, setBodyPart }) {
   const [search, setSearch] = useState('');
    const [bodyParts, setBodyParts] = useState([]);
-   
+  
 
- 
+   useEffect(() =>{
+    const fetchExercisesData = async () => {
+    const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
 
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+       fetchExercisesData();
+   },[]);
+
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (item) => item.name.toLowerCase().includes(search)
+               || item.target.toLowerCase().includes(search)
+               || item.equipment.toLowerCase().includes(search)
+               || item.bodyPart.toLowerCase().includes(search),
+      );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  }
 
   return (
     <div>
